aacdc56158cba3d9f8dddce36864486b628dec28
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-11 14:35:01 +0900

    feat : Header view 제작
    Header view 제작
    Container 컴포넌트 생성 : content 틀

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import Router from "./shared/Router";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return <Router />;
+};
 
 export default App;
---
+++ src/components/Container.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 95rem;
+  margin: 0 auto;
+  padding: 1.5rem 0;
+  box-sizing: border-box;
+`;
+
+export default Container;
